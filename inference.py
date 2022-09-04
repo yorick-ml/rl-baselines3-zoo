c06770c748f32d432fd88bd4d58fd74c9886c83d
vyorick <vyorick@gmail.com>
2022-09-04 09:19:47 +0300

    add inference

---
+++ inference.py
@@ -0,0 +1,60 @@
+import argparse
+
+import numpy as np
+from stable_baselines3.common.env_util import make_vec_env
+import utils.import_envs  # noqa: F401 pytype: disable=import-error
+from utils.utils import ALGOS
+
+
+def evaluate(env, model, num_steps=1000):
+    """
+    Evaluate a RL agent
+    :param model: (BaseRLModel object) the RL Agent
+    :param num_steps: (int) number of timesteps to evaluate it
+    :return: (float) Mean reward for the last 100 episodes
+    """
+    episode_rewards = [0.0]
+    obs = env.reset()
+    for i in range(num_steps):
+        # _states are only useful when using LSTM policies
+        action, _states = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+
+        # Stats
+        episode_rewards[-1] += reward
+        if done:
+            # DO NOT NEED env.reset() HERE!
+            episode_rewards.append(0.0)
+    # Compute mean reward for the last 100 episodes
+    mean_100ep_reward = round(float(np.mean(episode_rewards[-100:])), 3)
+    print("Mean reward:", mean_100ep_reward, "Num episodes:", len(episode_rewards))
+
+    return mean_100ep_reward
+
+
+def get_prediction():
+    obs = {
+            "grid": (0, 0, 0, 0, -2, 0, 0, 0, 0, -2, 0, 0, 0, 0, -2, 0, 0, 0, 0, -2, -2, -2, -2, -2, -2),
+            "positions": ((9, 9), (0, 0)),
+            "modificators": (0, 0, 0, 0, 0),
+            "my_mods": (1, 0, 0, 0),
+            "opp_mods": (1, 0, 0, 0),
+        }
+    action, _states = model.predict(obs)
+    print("action=", action)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algo", help="RL Algorithm", default="ppo", type=str, required=False,
+                        choices=list(ALGOS.keys()))
+    parser.add_argument("--env", type=str, default="JavaBot-v1", help="environment ID")
+    parser.add_argument("--model-path", help="Path to model", type=str, default="logs/ppo/JavaBot-v1_44/rl_model_999999_steps")
+    parser.add_argument("--eval-episodes", help="Number of episodes to use for evaluation", default=5, type=int)
+    args = parser.parse_args()
+
+    env_id = args.env
+    vec_env = make_vec_env(env_id, n_envs=1)
+    model = ALGOS[args.algo].load(args.model_path, device='cpu')
+    # evaluate(vec_env, model)
+    get_prediction()
